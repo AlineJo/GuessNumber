64702dd1e93bd2d45dbf37956eaa4f6d1eba47c2
yousuf <josephali1993@gmail.com>
2021-09-28 17:42:14 +0400

    #MyTextInput: Allow dynamic add for props

--- Componenets/MyTextInput.js
+++ Componenets/MyTextInput.js
@@ -8,21 +8,21 @@ import Spacing from '../Constants/Spacing';
 const MyTextInput = props => {
     return (
         <TextInput 
-        placeholder={props.hint} 
-        style={{ ...styles.input, ...props.style }} 
-        keyboardType={props.keyboardType}
-        onChangeText={props.onChangeText}/>
+        {...props}
+        style={{ ...styles.input, ...props.style }}/>
     )
 };
 
 
 const styles = StyleSheet.create({
     input: {
-        height: Spacing.spaceInputHeight,
+         height: Spacing.spaceInputHeight,
         width: Spacing.spacePercent_100,
+        paddingHorizontal:Spacing.space_4,
         marginTop: Spacing.space_10,
         borderBottomWidth: 1,
         borderBottomColor: Colors.colorInputBorder,
+
     },
 
 });
