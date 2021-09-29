24d347fdf9a6f36edc6bc7303d521e432682f1ec
AlineJo <josephali1993@gmail.com>
2021-09-29 12:04:52 +0400

    # reformat

--- Componenets/MyTextInput.js
+++ Componenets/MyTextInput.js
@@ -7,18 +7,18 @@ import Spacing from '../Constants/Spacing';
 
 const MyTextInput = props => {
     return (
-        <TextInput 
-        {...props}
-        style={{ ...styles.input, ...props.style }}/>
+        <TextInput
+            {...props}
+            style={{ ...styles.input, ...props.style }} />
     )
 };
 
 
 const styles = StyleSheet.create({
     input: {
-         height: Spacing.spaceInputHeight,
+        height: Spacing.spaceInputHeight,
         width: Spacing.spacePercent_100,
-        paddingHorizontal:Spacing.space_4,
+        paddingHorizontal: Spacing.space_4,
         marginTop: Spacing.space_10,
         borderBottomWidth: 1,
         borderBottomColor: Colors.colorInputBorder,
