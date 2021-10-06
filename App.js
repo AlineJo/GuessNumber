965beb23f97957710054054e939547f951444425
AlineJo <josephali1993@gmail.com>
2021-10-06 16:36:49 +0400

    #App: useState for userChosenNumber,  startGameHandler, swap current screen!

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './Componenets/Header';
 import FirstScreen from './Screens/FirstScreen';
 import GameScreen from './Screens/GameScreen';
 
 export default function App() {
+
+  const [userNumber, setUserNumber] = useState()
+
+  const startGameHandler = (chosenNumber) => {
+    setUserNumber(chosenNumber)
+  }
+
+  let currentScreenContent = <FirstScreen  onStartGamePressed={startGameHandler}/>
+
+   if(userNumber){
+     currentScreenContent = <GameScreen   onNPCGuessCorrect={startGameHandler} chosenNumber={userNumber}/>
+   }
+
   return (
     <View style={styles.root}>
       <Header title="Gusse The Number" />
-      <FirstScreen />
-      <GameScreen />
+      {currentScreenContent}
     </View>
   );
 }
