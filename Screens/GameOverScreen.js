c4a05b8b3627d162fefcced48d2528a048f4ad2d
AlineJo <josephali1993@gmail.com>
2021-10-07 13:14:26 +0400

    +GameOverScreen

---
+++ Screens/GameOverScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+import Card from '../Componenets/Card';
+import MyNumberContainer from '../Componenets/MyNumberContainer';
+import Spacing from '../Constants/Spacing';
+
+
+const GameOverScreen = props => {
+    return (<View style={styles.root}>
+
+        <MyNumberContainer
+            cardStyle={styles.card}
+            cardTitle="Number of tries it took NPC to guess the correct number is"
+            value={props.numberOfTries}
+            buttonVisible={true}
+            buttonTitle="Try Again"
+            onButtonPressed={props.onTryAgain}
+        />
+
+    </View>);
+};
+
+
+const styles = StyleSheet.create({
+
+    root: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    card: {
+        marginTop: 0,
+        height: Spacing.space_200,
+        alignItems: 'center',
+    },
+});
+
+
+export default GameOverScreen;
