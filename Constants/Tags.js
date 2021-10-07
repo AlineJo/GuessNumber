0adc1ebc0f5ad85a5b09e21b710d42b3320fd8d9
AlineJo <josephali1993@gmail.com>
2021-10-07 13:13:37 +0400

    +Tags.js

---
+++ Constants/Tags.js
@@ -0,0 +1,12 @@
+const Tags = {
+
+    screenContent_FirstScreen :"FirstScreen",
+    screenContent_GameScreen :"GameScreen",
+    screenContent_GameOverScreen :"GameOverScreen",
+    
+
+
+}
+
+
+export default Tags
