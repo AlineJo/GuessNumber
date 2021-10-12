8fd9cb156db9410e6e5cc1b387ee150e28a5d0af
AlineJo <josephali1993@gmail.com>
2021-10-12 14:55:00 +0400

    #MyText: reformat

--- Componenets/MyText.js
+++ Componenets/MyText.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native'
-import MyTextStyle from '../assets/styles/MyTextStyle';
-import Spacing from '../Constants/Spacing';
-import Tags from '../Constants/Tags';
-
-
+import { Text } from 'react-native'
 
 const MyText = props => {
 
-    return (<Text style={{...MyTextStyle(props.color).thin_12, ...props.style }}>{props.children}</Text>);
+    return (<Text style={props.style }>{props.children}</Text>);
 };
-
 
 
 
