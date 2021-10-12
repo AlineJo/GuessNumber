5d35ab9b3623ae5b9b207993de3b0250abc3df56
AlineJo <josephali1993@gmail.com>
2021-10-12 11:34:44 +0400

    #Tags:  +font tags

--- Constants/Tags.js
+++ Constants/Tags.js
@@ -1,9 +1,12 @@
 const Tags = {
 
-    screenContent_FirstScreen :"FirstScreen",
-    screenContent_GameScreen :"GameScreen",
-    screenContent_GameOverScreen :"GameOverScreen",
-    
+    screenContent_FirstScreen: "FirstScreen",
+    screenContent_GameScreen: "GameScreen",
+    screenContent_GameOverScreen: "GameOverScreen",
+
+    font_RobotoThin: 'font-roboto-thin',
+    font_RobotoLight: 'font-roboto-light',
+    font_RobotoBold: 'font-roboto-bold',
 
 
 }
