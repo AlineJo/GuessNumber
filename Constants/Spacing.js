0f1dda449b24abf0f55c7b45aabd560585d116f0
AlineJo <josephali1993@gmail.com>
2021-10-13 10:28:29 +0400

    #Spacing: new values

--- Constants/Spacing.js
+++ Constants/Spacing.js
@@ -1,34 +1,39 @@
 const Spacing = {
 
-    space_4:4,
-    space_8:8,
-    space_10:10,
-    space_16:16,
-    space_24:24,
-    space_32:32,
-    space_40:40,
-    space_96:96,
-    space_104:104,
-    space_132:132,
-    space_200:200,
+    space_4: 4,
+    space_8: 8,
+    space_10: 10,
+    space_16: 16,
+    space_24: 24,
+    space_32: 32,
+    space_40: 40,
+    space_96: 96,
+    space_100: 100,
+    space_104: 104,
+    space_132: 132,
+    space_200: 200,
+    space_224: 224,
+    space_300: 300,
 
 
 
-    spaceActionBarPaddingTop:42,
-    spaceActionBarHeight:76,
-    spaceActionBarPaddingBottom:16,
+    spaceActionBarPaddingTop: 42,
+    spaceActionBarHeight: 76,
+    spaceActionBarPaddingBottom: 16,
 
     spaceInputHeight: 36,
 
-    spaceCardRaduis:8,
-    spaceCardElevation:8,
+    spaceCardRaduis: 8,
+    spaceCardElevation: 8,
 
-    spacePercent_40:'40%',
-    spacePercent_95:'95%',
-    spacePercent_100:'100%',
+    spacePercent_25: '25%',
+    spacePercent_40: '40%',
+    spacePercent_50: '50%',
+    spacePercent_95: '95%',
+    spacePercent_100: '100%',
 
-    
-    fontSize_18 :18,
+
+    fontSize_18: 18,
 
     fontSize_Small_12: 12,
     fontSize_Small_14: 14,
