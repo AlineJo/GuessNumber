518094974b80835356682330083081575f1cbc6b
AlineJo <josephali1993@gmail.com>
2021-10-13 18:07:43 +0400

    #GameOverScreen: use Circle img

--- Screens/GameOverScreen.js
+++ Screens/GameOverScreen.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native'
+import MyCircleImage from '../Componenets/MyCircleImage';
 import MyNumberContainer from '../Componenets/MyNumberContainer';
-import Colors from '../Constants/Colors';
 import Spacing from '../Constants/Spacing';
+import Tags from '../Constants/Tags';
 
 
 const GameOverScreen = props => {
     return (<View style={styles.root}>
 
-        <View style={styles.imgContainer}>
-            <Image
-                style={styles.img} source={require('../assets/imgs/happyPC.png')} />
-
-        </View>
-
+        <MyCircleImage imgContainer={styles.imgContainer} src={Tags.img_happyPC} />
         <MyNumberContainer
             cardStyle={styles.card}
             cardTitle="Number of tries it took NPC to guess the correct number is"
@@ -40,34 +36,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
-    imgContainer: {
-        height: Spacing.space_200,
-        width: Spacing.space_200,
-        marginVertical: Spacing.space_16,
-        backgroundColor: Colors.colorWhite,
-
-        borderRadius: Spacing.space_100,
-        borderWidth: 1,
-        borderTopColor: Colors.colorCardBorderColor,
-        borderStartColor: Colors.colorCardBorderColor,
-        borderEndColor: Colors.colorCardBorderColor2,
-        borderBottomColor: Colors.colorCardBorderColor2,
-        overflow: 'hidden',
-
-        elevation: Spacing.spaceCardElevation,
-        shadowColor: Colors.colorBlack,
-        shadowOffset: {
-            width: 0,
-            height: 0
-        },
-        shadowRadius: 200,
-        shadowOpacity: 0.26
+    imgContainer:{
+        marginTop:Spacing.space_32,
     },
 
-    img: {
-        height: Spacing.spacePercent_100,
-        width: Spacing.spacePercent_100,
-    },
+
 });
 
 
