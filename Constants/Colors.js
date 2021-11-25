8ace69e01e97e67955bc158be0af0afc51341b16
AlineJo <josephali1993@gmail.com>
2021-11-25 17:55:08 +0400

    new value colorRipple

--- Constants/Colors.js
+++ Constants/Colors.js
@@ -1,25 +1,29 @@
+import { Platform } from "react-native";
 
 const Colors = {
 
-    colorPrimary:'#34EE99',
-    colorSecondary:'#3499EE',
+    colorPrimary: '#34EE99',
+    colorSecondary: '#3499EE',
 
-    colorActionBarTitleColor:'white',
+    colorActionBarTitleColor: Platform.OS === 'android' ? 'white' : 'black',
+    colorActionBarBackgroundColor: Platform.OS === 'android' ? '#34EE99' : 'white',
+    colorActionBarBorderColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
 
-    colorButtonPrimary:'#34EE99',
-    colorButtonSecondary:'#3499EE',
-    colorButtonCancel:'#EE5555',
+    colorButtonPrimary: '#34EE99',
+    colorButtonSecondary: '#3499EE',
+    colorButtonCancel: '#EE5555',
 
     colorBlack: 'black',
     colorWhite: 'white',
     colorGray: 'gray',
 
-    colorCardBorderColor:'#f8f8f8',
-    colorCardBorderColor2:'#f0f0f0',
+    colorCardBorderColor: '#f8f8f8',
+    colorCardBorderColor2: '#f0f0f0',
 
-    colorInputBorder:'#c3c3c3',
-    colorPlaceholderText:'#777',
+    colorInputBorder: '#c3c3c3',
+    colorPlaceholderText: '#777',
 
+    colorRipple: '#FFFFFFAA',
 
 
 
