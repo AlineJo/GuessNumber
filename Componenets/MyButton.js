1cea980fea086089d7f39bcf328d7c151ce48c1b
AlineJo <josephali1993@gmail.com>
2021-11-25 17:56:00 +0400

    show ripple effect if device is android and version is >= 21

--- Componenets/MyButton.js
+++ Componenets/MyButton.js
@@ -1,28 +1,44 @@
-
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, StyleSheet, Platform, TouchableNativeFeedback } from 'react-native'
 import MyTextStyle from '../assets/styles/MyTextStyle';
 import Colors from '../Constants/Colors';
 import Spacing from '../Constants/Spacing';
 import MyText from './MyText';
 
 
-const MyButton = props => {
-    return (<View>
+const ui = props => {
+    const content = <MyText
+        style={{
+            ...styles.buttonText,
+            ...styles.textStyle,
+            ...MyTextStyle(Colors.colorWhite).big_16
+        }}>
+        {props.title} {props.children}</MyText>;
 
-        <TouchableOpacity style={{ ...styles.button, ...props.style }} onPress={props.onPress} >
-            <View>
-                <MyText
-                    style={{
-                        ...styles.buttonText,
-                        ...styles.textStyle,
-                        ...MyTextStyle(Colors.colorWhite).big_16
-                    }}>
-                    {props.title} {props.children}</MyText>
+    let myButton = <TouchableOpacity style={{ ...styles.button, ...props.style }} onPress={props.onPress} >
+        <View>
+            {content}
+        </View>
+    </TouchableOpacity>;
+
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        myButton = <TouchableNativeFeedback onPress={props.onPress}
+            background={TouchableNativeFeedback.Ripple(Colors.colorRipple, false)}>
+
+            <View style={{ ...styles.button, ...props.style }}>
+                {content}
             </View>
-        </TouchableOpacity>
+        </TouchableNativeFeedback>;
+    }
 
-    </View>);
+    return myButton
+
+}
+
+
+
+const MyButton = props => {
+    return (<View>{ui(props)}</View>);
 };
 
 
